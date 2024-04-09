1f67b0c970dc7a95755cd0c8e1e19667e4470acc
hajjamn <hajjamn@gmail.com>
2024-04-09 15:00:23 +0200

    ADD: exercise ahah forgot to commit at all

---
+++ vite-hello-world-project/src/components/AppHero.vue
@@ -0,0 +1,35 @@
+<script>
+  //JS
+</script>
+
+<template>
+  <!-- HTML -->
+  <div id="gerry-wrapper">
+    <div id="gerry-card">
+      <h2 id="gerry-title">I suoi risotti</h2>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #gerry-wrapper {
+    width: 100%;
+    height: 900px;
+    background-image: url(../src/assets/img/gerry.webp);
+    background-size: cover;
+    background-position: center;
+    padding: 30px 0;
+  }
+  #gerry-card {
+    background-color: black;
+    width: 66%;
+    margin: 0 auto;
+    border-radius: 15px;
+  }
+  #gerry-title {
+    color: white;
+    font-size: 3rem;
+    margin-bottom: 0;
+  }
+</style>
--- vite-hello-world-project/src/components/AppTitle.vue
+++ vite-hello-world-project/src/components/AppTitle.vue
@@ -2,3 +2,11 @@
   //JS
 </script>
 
+<template>
+  <!-- HTML -->
+  <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+  /* CSS */
+</style>
